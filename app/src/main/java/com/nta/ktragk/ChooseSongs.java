0f6c39f4536ace5bed7ee44cb5d829ee2b77083b
KAIZER <nguyentienanh2907@gmail.com>
2021-11-20 22:49:09 +0700

    kiem tra giua ki

--- app/src/main/java/com/nta/ktragk/ChooseSongs.java
+++ app/src/main/java/com/nta/ktragk/ChooseSongs.java
@@ -4,16 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class ChooseSongs extends AppCompatActivity {
+public class ChooseSongs extends AppCompatActivity implements View.OnClickListener {
 
-    Button
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btnChon;
+    TextView txtChoose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_songs);
+        getViews();
         Intent intent = getIntent();
+    }
+
+    private void getViews() {
+
+        txtChoose = findViewById(R.id.txtChoose);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn1:
+                txtChoose.setText("1");
+                break;
+            case R.id.btn2:
+                txtChoose.setText("2");
+                break;
+            case R.id.btn3:
+                txtChoose.setText("3");
+                break;
+            case R.id.btn4:
+                txtChoose.setText("4");
+                break;
+            case R.id.btn5:
+                txtChoose.setText("5");
+                break;
+            case R.id.btn6:
+                txtChoose.setText("6");
+                break;
+            case R.id.btnChon:
+                Intent intent = new Intent();
+                intent.putExtra("choose", txtChoose.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+        }
     }
 }
--- app/src/main/java/com/nta/ktragk/MainActivity.java
+++ app/src/main/java/com/nta/ktragk/MainActivity.java
@@ -14,48 +14,97 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
     MediaPlayer music;
-    Button btnChoose, btnPlay,btnPause;
+    Button btnChoose, btnPlay, btnPause;
     TextView txtNumber, txtName, txtTime;
+    int[] idSongs = {
+            R.raw.easy_on_me,
+            R.raw.fancy_like,
+            R.raw.industry_baby,
+            R.raw.kiss_me_more,
+            R.raw.need_to_know,
+            R.raw.stay
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getViews();
-
     }
 
-    public void playMusic(View v){
+    public void playMusic(View v) {
         music.start();
     }
 
-    public  void pauseMusic(View v){
+    public void pauseMusic(View v) {
         music.pause();
     }
 
-    public void chooseSongs(View v){
-        Intent intent = new Intent(this,ChooseSongs.class);
-        chooseSongs.launch(intent);
+    public void chooseSongs(View v) {
+        Intent intent = new Intent(this, ChooseSongs.class);
+        activityLauncher.launch(intent);
     }
 
-    ActivityResultLauncher<Intent> chooseSongs = registerForActivityResult(
+    ActivityResultLauncher<Intent> activityLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
-                        @Override
-                        public void onActivityResult(ActivityResult result) {
-                            Log.d(TAG,)
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent intent = result.getData();
+                        if (intent != null) {
+                            String choose = intent.getStringExtra("choose");
+
+                            switch (choose) {
+                                case "1":
+                                    viewSong(idSongs[0],choose,"Easy On Me");
+                                    break;
+                                case "2":
+                                    viewSong(idSongs[1],choose,"Fancy Like");
+                                    break;
+                                case "3":
+                                    viewSong(idSongs[2],choose,"Industry baby");
+                                    break;
+                                case "4":
+                                    viewSong(idSongs[3],choose," Kiss me more");
+                                    break;
+                                case "5":
+                                    viewSong(idSongs[4],choose,"Need To Know");
+                                    break;
+                                case "6":
+                                    viewSong(idSongs[5],choose,"Stay");
+                                    break;
+                            }
                         }
                     }
-            );
+                }
+            }
+    );
 
-    private void getViews(){
+    public void viewSong(int id,String number,String name) {
+        if(music.isPlaying()){
+            music.release();
+        }
+        music = MediaPlayer.create(this, id);
+        double finalTime = music.getDuration();
+        txtNumber.setText(number);
+        txtName.setText(name);
+        txtTime.setText(String.format("%02d min, %02d sec",
+                TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) finalTime) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) finalTime)))
+        );
+    }
+
+    private void getViews() {
         btnChoose = findViewById(R.id.btnChoose);
         btnPlay = findViewById(R.id.btnPlay);
         btnPause = findViewById(R.id.btnPause);
         txtNumber = findViewById(R.id.txtNumber);
-        txtName= findViewById(R.id.txtName);
+        txtName = findViewById(R.id.txtName);
         txtTime = findViewById(R.id.txtTime);
     }
 }
